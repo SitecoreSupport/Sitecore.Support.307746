1a56354e7f352be54645fba0a7cdf0614ea950db
Sergey Plashenko <sergey.plashenko@sitecore.com>
2019-01-31 13:12:07 +0200

    Patch for the issue.
    New processor was added to set correct workflow and publish the host item after cancelling the test.

---
+++ src/Sitecore.Support.307746/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.307746")]
+[assembly: AssemblyProduct("Sitecore.Support.307746")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.307746/SetWorkflowToFinalStateAndPublish.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Sitecore.ContentTesting.Helpers;
+using Sitecore.ContentTesting.Pipelines.RevertContent;
+using Sitecore.Data.Items;
+using Sitecore.Publishing;
+
+namespace Sitecore.Support
+{
+  public class SetWorkflowToFinalStateAndPublish : RevertContentProcessor
+  {
+    public override void Process(RevertContentArgs args)
+    {
+      var hostItemUri = args.TestDefinition.ParseContentItem();
+      var hostItem =
+        args.TestDefinition.Database.GetItem(hostItemUri.ItemID, hostItemUri.Language, hostItemUri.Version);
+      if (hostItem != null)
+      {
+        var state = hostItem.State.GetWorkflowState();
+        using (new EditContext(args.HostItem))
+        {
+          if (state != null)
+          {
+            args.HostItem.Fields[FieldIDs.WorkflowState].SetValue(state.StateID, false);
+          }
+          else
+          {
+            args.HostItem.Fields[FieldIDs.WorkflowState].SetValue(string.Empty, false);
+          }
+        }
+      }
+
+      var targets = PublishingHelper.GetTargets(args.HostItem);
+      PublishManager.PublishItem(args.HostItem, targets.ToArray(), args.HostItem.Languages, false, false);
+    }
+  }
+}
